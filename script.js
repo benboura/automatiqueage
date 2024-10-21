e2af149be2f6bea3a0e107cc31286b30d4912b28
benboura <bourabenraphael@gmail.com>
2024-10-21 16:00:35 +0300

    21/10

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+
+// ajouter un ecouteur d'évenement pour le formulaire avec l'ID 'userForm'.
+
+document.getElementById('userForm').addEventListener('submit', (event) => {
+
+    event.preventDefault(); // Empêche le rechargement de la page lors de la soumission du formulaire .
+
+    const birthYear = parseInt(document.getElementById('birthYear').value); // Récupère l'année de naissance saisie par l'utilisateur et la convertir en entier. 
+
+    const currentYear = new Date().getFullYear(); // Récupère l'année actuelle à partir de l'objet Date. 
+
+    const age = currentYear - birthYear; // on Calcule l'âge en soustrayant l'année de la naissance de l'année actuelle.
+
+    document.getElementById('result').innerText = `Vous avez ${age} ans.`; // Affiche l'âge avec l'ID 'result'.
+    
+    
+});
+
+
+
 document.getElementById('userForm').addEventListener('submit', function(event) {
 
     event.preventDefault(); // Empêche le rechargement de la page
+
+    const firstName = document.getElementById('firstName').value; // Récupère le prénom
+
+    const lastName = document.getElementById('lastName').value; // Récupère le nom
 
     const birthYear = parseInt(document.getElementById('birthYear').value); // Récupère l'année de naissance
 
     const currentYear = new Date().getFullYear(); // Récupère l'année actuelle
 
-    const age = currentYear - birthYear; // on Calcule l'âge
+    const age = currentYear - birthYear; // Calcule l'âge
 
-    document.getElementById('result').innerText = `Vous avez ${age} ans.`; // Affiche l'âge 
+    document.getElementById('result').innerText = `Vous avez ${age} ans.`; // Affiche l'âge
+
+    console.log(`Nom: ${lastName}, Prénom: ${firstName}, âge: ${age}`); // Affiche le nom et prénom dans la console
 
 });
